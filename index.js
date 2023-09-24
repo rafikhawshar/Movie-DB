fb202ae7f56a6283413dd4a586cce34bcfbdd6a7
rafikxhawshar <rafikhawshar@gmail.com>
2023-09-24 12:09:10 +0300

    Step 3

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 // Making the app works with express
-const express = require ('express');
- const port = 5000 ;
- const app = express ();
+const express = require("express");
+const port = 5000;
+const app = express();
 
-// when opennig the local host it answers with ok
-app.get('/', (req,res) => {
-    res.send('Ok');
-
+// when opennig the local it answers with ok
+app.get("/", (req, res) => {
+  res.send("Ok ");
 });
 
-app.listen (5000, ()=> {
-    console.log('running the server ')
+app.listen(5000, () => {
+  console.log("running the server ");
+});
+
+app.get("/test", (req, res) =>{
+
+    res.send({ status: 200, message: "ok" })
+})
+app.get('/time',(req,res)=>{
+    const currentDate = new Date ();
+    res.send({status:200, message:`${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`})
 })
